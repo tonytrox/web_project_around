5284c6d65808ff816516edfea131896563efa156
Gilbert Carrasco <tonytrox@gmail.com>
2023-11-21 15:53:51 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Tripleten web_project_around
+
+En este proyecto se intenta alcanzar la experincia con respecto a objetos de ingreso (input).
+
+Para este trabajo se usó las funcionalidades de Flex y Grid, la ruptura de pantalla en 720px y en mejora.
+
+Con respecto a las instrucción de JavaScript, estas fueron otorgando acciones. 
+Lo adjunto el proyecto para su evalución y feedback.
+
+En desarrollo
+Link
---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+// open/close form
+
+const formElement = document.querySelector(".form");
+const editButton = document.querySelector(".profile__edit-button");
+const exitButton = document.querySelector(".form__exit-button");
+const saveButton = document.querySelector(".form__save-button");
+
+function ChangeVisibility() {
+  formElement.classList.toggle('form_view');
+}
+
+function HideVisibility() {
+  formElement.classList.remove('form_view');
+}
+
+editButton.addEventListener("click", ChangeVisibility);
+exitButton.addEventListener('click', HideVisibility);
+
+
+
+// Edit Profile
+
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__description");
+let nameInput = document.querySelector(".form__name");
+let jobInput = document.querySelector(".form__description");
+
+nameInput.placeholder = profileName.textContent;
+jobInput.placeholder = profileJob.textContent;
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+}
+
+saveButton.addEventListener("click", handleProfileFormSubmit);
